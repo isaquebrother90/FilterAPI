fddb558f46ba247c24ec7f74cc543a8154e87c2c
isaquebrother90 <improgramcode@gmail.com>
2025-02-17 01:40:27 -0300

    feat: ajustes swagger e pom

--- src/main/java/com/filterapi/config/SwaggerConfig.java
+++ src/main/java/com/filterapi/config/SwaggerConfig.java
@@ -7,20 +7,27 @@ import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 
 @Configuration
 public class SwaggerConfig {
+
+    @Autowired
+    private Environment environment;
+
     @Bean
     public OpenAPI customOpenAPI(@Value("${springdoc.api-docs.path}") String apiDocsPath) {
+        String devUrl = environment.getProperty("filterapi.openapi.dev-url");
         return new OpenAPI()
                 .info(new Info().title("FilterAPI")
                         .description("Microsserviço para filtro e geração de recomendação e relatórios de compras")
                         .version("1.0")
                         .license(new License().name("MIT License").url("https://choosealicense.com/licenses/mit/")))
-                .addServersItem(new Server().url("${filterapi.openapi.dev-url}"))
+                .addServersItem(new Server().url(devUrl))
                 .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
                 .components(new Components()
                         .addSecuritySchemes("bearerAuth", new SecurityScheme()
